8c8603c5c94758bc7bef5a7a60337a12a0e9cc04
Muhammad Julian <38089145+muhammadjulian@users.noreply.github.com>
2022-01-11 10:58:31 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { State } from './state/state.interface';
 import { completeToDos, incompleteToDos } from './state/todo';
-import { AddToDo, CompleteToDo, IncompleteToDo } from './state/todo/todo.actions';
+import {
+  AddToDo,
+  CompleteToDo,
+  IncompleteToDo,
+} from './state/todo/todo.actions';
 import { generateToDos, ToDo } from './state/todo/todo.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   completeToDos: Observable<Array<ToDo>>;
 
   incompleteToDos: Observable<Array<ToDo>>;
 
   private _toDo: Partial<ToDo>;
 
-  constructor(private store: Store<State>) { }
-  
-  initialize() {
-    generateToDos().forEach(todo => this.store.dispatch(new AddToDo(todo)));
-    this.completeToDos = this.store.pipe(completeToDos);
-    this.incompleteToDos = this.store.pipe(incompleteToDos);
+  constructor(private store: Store<State>) {}
+
+  ngOnInit() {
+    generateToDos().forEach((todo) => this.store.dispatch(new AddToDo(todo)));
+    this.completeToDos = this.store.pipe(select(completeToDos));
+    this.incompleteToDos = this.store.pipe(select(incompleteToDos));
   }
 
   addToDo() {
-    this.store.dispatch(new AddToDo({
-      id: Math.random(),
-      complete: false,
-      task: this._toDo.task
-    }));
+    this.store.dispatch(
+      new AddToDo({
+        id: Math.random(),
+        complete: false,
+        task: this._toDo.task,
+      })
+    );
   }
 
-  onAddToDoChange() {
+  onAddToDoChange(toDo: Partial<ToDo>) {
+    this._toDo = toDo;
   }
 
   onCompleteToDo(toDo: ToDo) {
